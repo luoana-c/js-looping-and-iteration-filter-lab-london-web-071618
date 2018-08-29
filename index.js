810b76ed5b89d5374db5e95f56ace00b20a0b04f
Luoana Chirita <luoana.livadariu@gmail.com>
2018-08-29 13:51:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+function findMatching(collection, name){
+  
+  return collection.filter(function(driver) {
+    return driver.toLowerCase() === name.toLowerCase()
+  })
+}
+
+function fuzzyMatch(collection, stringToMatch) {
+  return collection.filter(function(driver) {
+    return driver.slice(0, stringToMatch.length) === stringToMatch
+  })
+}
+
+function matchName(collection, stringToMatch) {
+  return collection.filter(function(driver) {
+    return driver.name === stringToMatch
+  })
+}
